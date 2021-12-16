c375f781bee6a491cf968d2e6911a1b742ac979f
heraldofortuna <heraldofortuna@gmail.com>
2021-12-16 12:31:39 -0500

    React router dependencies were added to app

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,7 +4,11 @@ const CharacterList = ({ characters }) => {
   if (characters === null) return null;
 
   return characters.map((character) => (
-    <div key={character.id}>
+    <div
+      key={character.id}
+      onClick={() => {}}
+      style={{ border: "1px solid red" }}
+    >
       <h2>{character.name}</h2>
       <p>
         {character.species === null ? "Human" : character.species.name} from{" "}
