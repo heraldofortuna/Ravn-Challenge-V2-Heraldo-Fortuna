f714b06dc944b1ad1c6b94239309a3df87c44284
heraldofortuna <heraldofortuna@gmail.com>
2021-12-16 17:45:27 -0500

    Real data was passed to Character component

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -5,11 +5,41 @@ import Header from "../components/Header";
 const Character = ({ characters }) => {
   const id = useParams().id;
   const character = characters.find((character) => character.id === id);
-
+  console.log(character);
   return (
     <>
       <Header title={character.name} canReturn />
-      <h3>{character.name}</h3>
+      <main>
+        <div className="container">
+          <h2>General Information</h2>
+          <ul>
+            <li>
+              <h3>Eye Color</h3>
+              <p>{character.eyeColor}</p>
+            </li>
+            <li>
+              <h3>Hair Color</h3>
+              <p>{character.hairColor}</p>
+            </li>
+            <li>
+              <h3>Skin Color</h3>
+              <p>{character.skinColor}</p>
+            </li>
+            <li>
+              <h3>Birth Year</h3>
+              <p>{character.birthYear}</p>
+            </li>
+          </ul>
+          <h2>Vehicles</h2>
+          <ul>
+            {character.vehicleConnection.vehicles.map((vehicle) => (
+              <li>
+                <h3>{vehicle.name}</h3>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </>
   );
 };
