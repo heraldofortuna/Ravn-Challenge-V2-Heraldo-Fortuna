eb9af78ef1e40692f949731c1eb88774b6b896fc
heraldofortuna <heraldofortuna@gmail.com>
2022-09-19 01:36:06 -0500

    Instalamos el gh-pages para hacer deployar la app. Para esto, hacemos la config correspondiente en el package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
